1540d844be2cb16d959791afade29b548e7dc21c
Nikita Rai <91018857+vinnk@users.noreply.github.com>
2023-06-12 18:11:56 +0530

    Assignment4

--- MazePathProblem.java
+++ MazePathProblem.java
@@ -3,24 +3,89 @@ import java.util.*;
 
 public class MazePathProblem {
 
-	static ArrayList<String> maze(int R, int C, int i, int j, String ans, ArrayList<String> list){
-		if(i>R || j>C || i<0 || j<0)			//as left and up moved are not defined
-			return list;
+	static void maze(int R, int C, int i, int j, String ans){
+		if(i>R || j>C)			//as left and up moved are not defined
+			return;
 		if(i==R && j==C) {							//base case
-			list.add(ans);
+			System.out.println(ans);
+			return;
+		}
+		maze(R, C, i+1, j, ans+"R");			//small problem
+		maze(R, C, i, j+1, ans+"C");			//small problem
+		
+	}
+	
+	static ArrayList<String> maze(int R, int C, int i, int j){
+		if(i==R && j==C) {
+			ArrayList<String> list= new ArrayList<>();
+			list.add("");
 			return list;
 		}
-		maze(R, C, i+1, j, ans+"R", list);			//small problem
-		maze(R, C, i, j+1, ans+"C", list);			//small problem
+		if(i>R || j>C) {					//as left and up moves are not defined for this method
+			ArrayList<String> list= new ArrayList<>();
+			return list;
+		}
 		
-		return list;
+		ArrayList<String> result= new ArrayList<>();
+		ArrayList<String> down= maze(R, C, i+1, j);
+		for(String s : down)
+			result.add("D"+s);
+		
+		ArrayList<String> right= maze(R, C, i, j+1);
+		for(String s : right)
+			result.add("R"+s);
+		
+		return result;
+	}
+	
+	static void mazeDiagonal(int R, int C, int i, int j, String ans){
+		if(i>R || j>C)			//as left and up moved are not defined
+			return;
+		if(i==R && j==C) {							//base case
+			System.out.println(ans);
+			return;
+		}
+		mazeDiagonal(R, C, i+1, j, ans+"R");			//small problem
+		mazeDiagonal(R, C, i, j+1, ans+"C");			//small problem
+		mazeDiagonal(R, C, i+1, j+1, ans+"I");
+		
+	}
+	
+	static ArrayList<String> mazeDiagonal(int R, int C, int i, int j){
+		if(i==R && j==C) {
+			ArrayList<String> list= new ArrayList<>();
+			list.add("");
+			return list;
+		}
+		if(i>R || j>C) {					//as left and up moves are not defined for this method
+			ArrayList<String> list= new ArrayList<>();
+			return list;
+		}
+		
+		ArrayList<String> result= new ArrayList<>();
+		ArrayList<String> down= mazeDiagonal(R, C, i+1, j);
+		for(String s : down)
+			result.add("D"+s);									//as while backtracking, the previous move are added before the returned value
+		
+		ArrayList<String> right= mazeDiagonal(R, C, i, j+1);
+		for(String s : right)
+			result.add("R"+s);
+		
+		ArrayList<String> diagonal= mazeDiagonal(R, C, i+1, j+1);
+		for(String s : diagonal)
+			result.add("I"+s);
+		
+		return result;
 	}
 	
 	public static void main(String[] args) {
-		ArrayList<String> list= new ArrayList<>();
 		String ans= "";
 		
-		System.out.println(maze(2,2,0,0,ans,list));
+		maze(2,2,0,0,ans);
+		System.out.println("\n"+maze(2,2,0,0)+"\n");
+		
+		mazeDiagonal(2,2,0,0,ans);
+		System.out.println("\n"+mazeDiagonal(2,2,0,0));
 	}
 
 }
--- diceGame.java
+++ diceGame.java
@@ -4,27 +4,54 @@ import java.util.*;
 
 public class diceGame {
 
-	static ArrayList<String> dice(int goal, int sum, String ans, ArrayList<String> list){
+	static void dice(int goal, int sum, String ans){
 		if(sum>goal) 							//base case
-			return list;
+			return;
 		if(sum==goal) {
-			list.add(ans);
+			System.out.println(ans);
+			return;
+		}
+		dice(goal, sum+1, ans+"1");		//small problem
+		dice(goal, sum+2, ans+"2");
+		dice(goal, sum+3, ans+"3");
+		dice(goal, sum+4, ans+"4");
+		dice(goal, sum+5, ans+"5");
+		dice(goal, sum+6, ans+"6");
+	
+		/*
+		 * for(int d=1;d<=6;d++){
+		 * 		dice(goal, sum+d, ans+d);
+		 * }
+		 */
+		
+	}
+	
+	static ArrayList<String> dice(int goal, int sum){
+		if(sum==goal) {
+			ArrayList<String> list= new ArrayList<>();
+			list.add("");
 			return list;
 		}
-		dice(goal, sum+1, ans+"1", list);		//small problem
-		dice(goal, sum+2, ans+"2", list);
-		dice(goal, sum+3, ans+"3", list);
-		dice(goal, sum+4, ans+"4", list);
-		dice(goal, sum+5, ans+"5", list);
-		dice(goal, sum+6, ans+"6", list);
+		if(sum>goal) {
+			ArrayList<String> list= new ArrayList<>();
+			return list;
+		}
 		
-		return list;
+		ArrayList<String> ans= new ArrayList<>();
+		for(int d=1;d<=6;d++) {
+			ArrayList<String> list= dice(goal, sum+d);
+			
+			for(String s : list) {
+				ans.add(s+d);
+			}
+		}
+		return ans;
 	}
 	
 	public static void main(String[] args) {
 		String ans= "";
-		ArrayList<String> list= new ArrayList<>();
-		System.out.println(dice(5,0,ans,list));
+		dice(5,0,ans);
+		System.out.println("\n"+dice(4,0));
 	}
 
 }
